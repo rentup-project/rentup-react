4f51b19278e91d845d30bae7757cbb110c8dca5c
Marina Laporte Boni <marinaolboni@gmail.com>
2022-10-19 21:18:37 +0200

    filter form created

--- src/components/PropertyList/PropertyList.jsx
+++ src/components/PropertyList/PropertyList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './PropertyList.css'
 
-export default function PropertyList({ images, address, bedroom, bathroom, id }) {
+export default function PropertyList({ images, address, bedroom, bathroom, id, squaredMeters }) {
     return (
         <Link to={`/property/${id}`} className="property-link">
             <div className='PropertyList' id={id}>
@@ -10,7 +10,17 @@ export default function PropertyList({ images, address, bedroom, bathroom, id })
                 </div>
                 <div className='content-div'>
                     <h4>{address.split(',')[0]}</h4>
-                    <p>{bedroom} bedrooms & {bathroom} bathrooms</p>
+                    <span>{squaredMeters} m2</span>
+                    <span>•</span>
+                    <span>
+                        {bedroom}
+                        {bedroom === "1" ? " bedroom" : " bedrooms"}
+                    </span>
+                    <span>•</span>
+                    <span>
+                        {bathroom}
+                        {bathroom === "1" ? " bathroom" : " bathrooms"}
+                    </span>
                 </div>
             </div>
         </Link>
