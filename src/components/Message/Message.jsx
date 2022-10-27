f11664a8e852185a4d00691ff10d19f05dce0fcb
Marina Laporte Boni <marinaolboni@gmail.com>
2022-10-27 15:14:54 +0200

    user and message services + message screen

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import { getOneUser } from './../../services/Users.services';
+import moment from "moment";
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getMessages } from './../../services/Messages.services';
 import { getCurrentUser } from '../../services/Auth.services';
+import { createMessage, getMessages } from './../../services/Messages.services';
+import { getOneUser } from './../../services/Users.services';
+import './Message.css';
 
 export default function Message( {ownerId} ) {
     const [currentUser, setCurrentUser] = useState('')
     const [owner, setOwner] = useState('')
     const [messages, setMessages] = useState([])
+    const [messageToSend, setMessageToSend] = useState('')
+    const [update, setUpdate] = useState(false)
     const navigate = useNavigate()
 
     useEffect(()=> {
@@ -18,17 +22,62 @@ export default function Message( {ownerId} ) {
             .then((currentUser) => {
                 setCurrentUser(currentUser)
                 getMessages(currentUser.id, owner.id)
-                .then((res) => setMessages(res))
+                .then((res) => {
+                    setMessages(res)
+                    setUpdate(false)
+                })
                 .catch((err) => navigate("/error"))
             })
             .catch((err) => navigate("/error"))
         })
         .catch((err) => navigate("/error"))
-    }, [])
+    }, [update])
+
+    const handleOnChange = (e) => {
+        const { value } = e.target
+        setMessageToSend(value) 
+    }
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+
+        const body = {
+            sender: currentUser.id,
+            receiver: owner.id,
+            msg: messageToSend
+        }
+
+        createMessage(body)
+        .then((res) => {
+            setUpdate(true)
+            setMessageToSend('')
+        })
+        .catch((err) => navigate("/error"))        
+    }
 
     return (
         <div>
-            AEEE
+            <div className="messages-scroll-container">
+                {
+                    [...messages].map((msg) => (
+                        <div key={msg._id} className={`each-message ${msg.sender.id === currentUser.id ? 'mine' : 'theirs'}`}>
+                            <p className='timestamp'>
+                                <span>
+                                    {msg.sender.name} -
+                                </span>
+                                <span>
+                                    {moment(msg.createdAt).format('DD/MM/YY - hh:mm')}
+                                </span>
+                            </p>
+                            <p className='text-content'>{msg.msg}</p>
+                        </div>  
+                    ))
+                }
+            </div>
+            <form onSubmit={handleOnSubmit} className='messages-form'>
+                <textarea id="msg" name="msg" rows="5" onChange={handleOnChange} value={messageToSend}></textarea>
+                <button>Send</button>
+            </form>
         </div>
     )
 }
--- src/services/Messages.services.js
+++ src/services/Messages.services.js
@@ -5,3 +5,6 @@ const noAuthHttp = createHttp();
 
 export const getMessages = (currentUser, owner) => 
 authHttp.get(`/messages/${currentUser}/${owner}`);
+
+export const createMessage = (body) => 
+authHttp.post(`/messages/create`, body);
